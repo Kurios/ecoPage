bc01f2d95dffdffc62d840af580786a655f040f0
Jack Morgan <jmorgan@aravo.com>
2020-06-26 15:24:51 -0700

    Add project files.

---
+++ ecoPage/ClientApp/src/app/eco/eco.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eco-home',
+  templateUrl: './eco.component.html',
+})
+export class EcoComponent {
+}
---
+++ ecoPage/ClientApp/src/app/eco/recipe/eco.recipe.js
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eco-recipe',
+  templateUrl: './eco.recipe.html',
+})
+export class EcoRecipeComponent {
+}
